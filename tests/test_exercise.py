d1309779542ae76bab3197c0674f0fcceb701a3d
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 14:02:06 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,5 +2,25 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    input_values = ["chelsea,2017","luke,2017","lily,2014","hanna,2010",""]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert output == ["","","","","Longest name: chelsea","Average of the birth years: 2014"]
