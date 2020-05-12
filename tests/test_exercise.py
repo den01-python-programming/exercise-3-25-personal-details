8977e1aa5302128bb2b5406ee0494223fca0a79c
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 11:27:26 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -18,9 +18,4 @@ def test_exercise():
 
     src.exercise.main()
 
-    src.exercise.input = mock_input
-    src.exercise.print = lambda s : output.append(s)
-
-    src.exercise.main()
-
-    assert output == ["","","","","Longest name: chelsea","Average of the birth years: 2014"]
+    assert output == ["","","","","","Longest name: chelsea","Average of the birth years: 2014.5"]
